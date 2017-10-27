02356b8c2eb203c410e5022ff6cf619cceb4d09f
Prabhu Nayidi <pabhun.python@gmail.com>
2017-10-26 19:41:16 -0500

    A sample test case to check whether admin was logged in or not.

--- SeleniumTesting/src/sanityTests/AdminLogin.java
+++ SeleniumTesting/src/sanityTests/AdminLogin.java
@@ -1,9 +1,29 @@
 package sanityTests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class AdminLogin {
 
 	public static void main(String[] args) {
-
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\PrabhuPushpaKumar\\Downloads\\SeleniumFiles\\geckodriver-v0.19.0-win64\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.gcrit.com/build3/admin/login.php");
+		driver.findElement(By.name("username")).sendKeys("admin");
+		driver.findElement(By.name("password")).sendKeys("admin@123");
+		driver.findElement(By.id("tdb1")).click();
+		
+		String url = driver.getCurrentUrl();
+		if (url.equals("http://www.gcrit.com/build3/admin/index.php"))
+		{
+			System.out.println("Admin Logged in - Test Case Passed");
+		}
+		else
+		{
+			System.out.println("Admin not logged in - Test Case Failed");
+		}
+		driver.close();
 	}
 
 }
