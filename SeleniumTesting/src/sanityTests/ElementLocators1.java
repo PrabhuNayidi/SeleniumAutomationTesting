0f4a96067f0960bb641b3cb79074814ee36e2e99
Prabhu Nayidi <pabhun.python@gmail.com>
2017-11-14 12:22:51 -0600

    Changes to the Google Chrome Web Browser commands code.

--- SeleniumTesting/src/sanityTests/ElementLocators1.java
+++ SeleniumTesting/src/sanityTests/ElementLocators1.java
@@ -2,6 +2,7 @@ package sanityTests;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -24,10 +25,15 @@ public class ElementLocators1 {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\PrabhuPushpaKumar\\Downloads\\SeleniumFiles\\chromedriver_win32\\chromedriver.exe");
 		WebDriver d2 = new ChromeDriver();
 		d2.get("https://www.facebook.com/login/identify?ctx=recover&lwv=110");
+		WebElement e1 = d2.findElement(By.xpath(".//*[@id='identify_email']"));
+		e1.sendKeys("newemail@gmail.com");
+		e1.clear();
+		
 		d2.findElement(By.xpath(".//*[@id='identify_email']")).sendKeys("sample.new@gmail.com");
 		d2.findElement(By.name("did_submit")).click();
+		
+		
 		d2.close();
-
 	}
 
 }
