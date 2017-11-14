dc44f03902d8c035d66c940d826b4f02523aff37
Prabhu Nayidi <pabhun.python@gmail.com>
2017-11-14 12:02:24 -0600

    Code for Automating the Google Chrome Web Browser was added to the ElementLocators1.java file.

--- SeleniumTesting/src/sanityTests/ElementLocators1.java
+++ SeleniumTesting/src/sanityTests/ElementLocators1.java
@@ -2,6 +2,7 @@ package sanityTests;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class ElementLocators1 {
@@ -18,6 +19,15 @@ public class ElementLocators1 {
 		/*d1.findElement(By.name("birthday_day")).sendKeys("5");
 		d1.findElement(By.name("birthday_year")).sendKeys("2000");
 		d1.findElement(By.xpath(".//*[@id='u_0_4']")).click();*/
+		d1.close();
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\PrabhuPushpaKumar\\Downloads\\SeleniumFiles\\chromedriver_win32\\chromedriver.exe");
+		WebDriver d2 = new ChromeDriver();
+		d2.get("https://www.facebook.com/login/identify?ctx=recover&lwv=110");
+		d2.findElement(By.xpath(".//*[@id='identify_email']")).sendKeys("sample.new@gmail.com");
+		d2.findElement(By.name("did_submit")).click();
+		d2.close();
+
 	}
 
 }
