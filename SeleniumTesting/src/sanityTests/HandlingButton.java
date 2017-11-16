624ed6abf6c07d671a65b9b6ef523ddbec56ef2f
Prabhu Nayidi <pabhun.python@gmail.com>
2017-11-15 18:00:35 -0600

    A Sample java code on Handling the Button Element was added to the HandlingButton.java file.

--- SeleniumTesting/src/sanityTests/HandlingButton.java
+++ SeleniumTesting/src/sanityTests/HandlingButton.java
@@ -1,9 +1,27 @@
 package sanityTests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class HandlingButton {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\PrabhuPushpaKumar\\Downloads\\SeleniumFiles\\chromedriver_win32\\chromedriver.exe");
+		WebDriver c1 = new ChromeDriver();
+		c1.get("https://www.facebook.com/");
+		c1.manage().window().maximize();
+		WebElement e1 = c1.findElement(By.id("u_0_r"));
+		System.out.println("The Enabled Status of the Specified Button is : " + e1.isEnabled());
+		c1.findElement(By.id("u_0_r")).click();
+		System.out.println("The Displayed Status of the Specified Button is : " + e1.isDisplayed());
+		System.out.println("The Enabled Status of the Specified Button is : " + e1.isEnabled());
+		System.out.println("The Type of the Specified Button is : " + e1.getAttribute("type"));
+		System.out.println("The Name of the Specified Button is : " + e1.getAttribute("name"));
 		
+		Thread.sleep(2000);
+		c1.quit();
 	}
 
 }
