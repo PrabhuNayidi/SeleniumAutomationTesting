bb1831b8751f6cfa51904bd641a3e64f9f824b54
Prabhu Nayidi <pabhun.python@gmail.com>
2017-11-16 14:28:31 -0600

    Sample Java Code on Handling a Radio Button in Selenium was added to HandlingRadioButton.java file.

--- SeleniumTesting/src/sanityTests/HandlingRadioButton.java
+++ SeleniumTesting/src/sanityTests/HandlingRadioButton.java
@@ -1,9 +1,28 @@
 package sanityTests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class HandlingRadioButton {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\PrabhuPushpaKumar\\Downloads\\SeleniumFiles\\geckodriver-v0.19.0-win64\\geckodriver.exe");
+		WebDriver f1 = new FirefoxDriver();
+		f1.get("https://www.bharatmatrimony.com/");
+		WebElement we1 = f1.findElement(By.xpath(".//*[@id='genderfemale']"));
+		System.out.println("The Enabled Status of the Specified Radio Button before Selecting it : " + we1.isEnabled());
+		System.out.println("The Displayed Status of the Specified Radio Button before Selecting it : " + we1.isDisplayed());
+		System.out.println("The Selected Status of the Specified Radio Button before Selecting it : " + we1.isSelected());
 		
+		we1.click();
+		System.out.println("The Enabled Status of the Specified Radio Button after Selecting it : " + we1.isEnabled());
+		System.out.println("The Displayed Status of the Specified Radio Button after Selecting it : " + we1.isDisplayed());
+		System.out.println("The Selected Status of the Specified Radio Button after Selecting it : " + we1.isSelected());
+		
+		Thread.sleep(4000);
+		f1.quit();
 	}
 
 }
