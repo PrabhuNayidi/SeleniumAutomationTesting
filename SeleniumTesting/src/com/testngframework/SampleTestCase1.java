9e7e709b2885baf63aa69758215c87ef630bd82e
Prabhu Nayidi <pabhun.python@gmail.com>
2018-01-30 16:10:24 -0600

    A Sample TestNG Test Case Code was added to the same SampleTestCase1.java file.

--- SeleniumTesting/src/com/testngframework/SampleTestCase1.java
+++ SeleniumTesting/src/com/testngframework/SampleTestCase1.java
@@ -2,10 +2,14 @@ package com.testngframework;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class SampleTestCase1 
 {
-	public static WebDriver f1;
+	// General Selenium WebDriver Test Case.
+	
+	/*public static WebDriver f1;
 	public static void main(String[] args) 
 	{
 		System.setProperty("webdriver.gecko.driver", "C:\\Users\\PrabhuPushpaKumar\\Downloads\\SeleniumFiles\\geckodriver-v0.19.0-win64\\geckodriver.exe");
@@ -19,6 +23,23 @@ public class SampleTestCase1
 			System.out.println("Test Passed!!");
 		else
 			System.out.println("Test Failed!");
+		
+		f1.quit();
+	}*/
+	
+	// TestNG Test Case.
+	
+	@Test 
+	public void titleCheck()
+	{
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\PrabhuPushpaKumar\\Downloads\\SeleniumFiles\\geckodriver-v0.19.0-win64\\geckodriver.exe");
+		WebDriver f1= new FirefoxDriver();
+		f1.manage().window().maximize();
+		f1.get("https://www.udemy.com/");
+		
+		String pageTitle = f1.getTitle();
+		
+		Assert.assertEquals(pageTitle, "Online Courses - Learn Anything, On Your Schedule | Udemy");
 		
 		f1.quit();
 	}
