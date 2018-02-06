118c73b4179a0bf96e85573474544bec5d3e0857
Prabhu Nayidi <pabhun.python@gmail.com>
2018-02-06 14:39:03 -0600

    Sample TestNG code was added to the TestCase4.java file.

--- SeleniumTesting/src/com/testngframework/TestCase4.java
+++ SeleniumTesting/src/com/testngframework/TestCase4.java
@@ -1,6 +1,44 @@
 package com.testngframework;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class TestCase4 
 {
-
+	  @BeforeMethod
+	  public void login()
+	  {
+		  System.out.println("You have logged into your Account Successfully!");
+	  }
+		
+	  @Test 
+	  public void checkUser() 
+	  {
+		  System.out.println("The User was checked among the Existing Records Successfully!");
+	  }
+	  
+	  @Test
+	  public void createUser() 
+	  {
+		  System.out.println("New User was added to the Existing Records Successfully!");
+	  }
+	  
+	  @Test
+	  public void editUser() 
+	  {
+		  System.out.println("The Selected User information was changed Successfully!");
+	  }
+	  
+	  @Test
+	  public void deleteUser() 
+	  {
+		  System.out.println("The Selected User was deleted from the Existing Records Successfully!");
+	  }
+	  
+	  @AfterMethod
+	  public void logout()
+	  {
+		  System.out.println("You have logged out of your Account Successfully!");
+	  }
 }
