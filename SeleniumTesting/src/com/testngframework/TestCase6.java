817e6241686c53040585f7a8bc18361fc2fcb9e8
Prabhu Nayidi <pabhun.python@gmail.com>
2018-02-07 10:49:12 -0600

    Code was added to TestCase6.java file and some content was added to the TestNG Framework Part II.txt file.

--- SeleniumTesting/src/com/testngframework/TestCase6.java
+++ SeleniumTesting/src/com/testngframework/TestCase6.java
@@ -5,8 +5,33 @@ import org.testng.annotations.Test;
 public class TestCase6 
 {
   @Test
-  public void f() 
+  public void tc1() 
   {
-	  
+	  long t_id = Thread.currentThread().getId();
+	  System.out.println("The Current Test Case 1 was passed successfully and the Thread ID for current Test Case is - " + t_id);
+  }
+  @Test
+  public void tc2() 
+  {
+	  long t_id = Thread.currentThread().getId();
+	  System.out.println("The Current Test Case 2 was passed successfully and the Thread ID for current Test Case is - " + t_id);
+  }
+  @Test
+  public void tc3() 
+  {
+	  long t_id = Thread.currentThread().getId();
+	  System.out.println("The Current Test Case 3 was passed successfully and the Thread ID for current Test Case is - " + t_id);
+  }
+  @Test
+  public void tc4() 
+  {
+	  long t_id = Thread.currentThread().getId();
+	  System.out.println("The Current Test Case 4 was passed successfully and the Thread ID for current Test Case is - " + t_id);
+  }
+  @Test
+  public void tc5() 
+  {
+	  long t_id = Thread.currentThread().getId();
+	  System.out.println("The Current Test Case 5 was passed successfully and the Thread ID for current Test Case is - " + t_id);
   }
 }
