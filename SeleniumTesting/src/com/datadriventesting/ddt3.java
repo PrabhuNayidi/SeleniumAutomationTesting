ba80e2132c1e66653202c391ffe82816f1f9998c
Prabhu Nayidi <pabhun.python@gmail.com>
2018-02-12 15:52:28 -0600

    Sample java code was added to ddt3.java file.

--- SeleniumTesting/src/com/datadriventesting/ddt3.java
+++ SeleniumTesting/src/com/datadriventesting/ddt3.java
@@ -1,6 +1,33 @@
 package com.datadriventesting;
 
-public class ddt3 
+import java.io.File;
+import java.io.IOException;
+
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
+public class ddt3
 {
+	public Object [] [] readData() throws BiffException, IOException
+	{
+		File f = new File("C:\\Users\\PrabhuPushpaKumar\\Desktop\\AdminDataFile2.xls");
+		Workbook w = Workbook.getWorkbook(f);
+		Sheet s = w.getSheet(0);
+		
+		int rows = s.getRows();
+		int columns = s.getColumns();
+		
+		String intdata [] [] = new String [5] [10];
+		
+		for (int i = 0; i < rows; i++)
+			for (int j = 0; j < columns; j++)
+			{
+				Cell c = s.getCell(j,i);
+				intdata [i] [j] = c.getContents();
+			}
+		return intdata;
+	}
 	
 }
